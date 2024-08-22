3851b690aa41a6b3dc998e0e923d0895a1f5ce8e
omar karake <omar.karake@amalitechtraining.org>
2024-08-22 17:45:41 +0200

    using spread operator and object.assign to maintain immutability

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
-  styleUrl: './counter-output.component.css',
+  styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent {
+export class CounterOutputComponent implements OnDestroy {
   counter = 0;
   counterServiceSub?: Subscription;
   count$: Observable<number>;
-  constructor(private store: Store<{ counter: number }>) {
-    this.count$ = store.select('counter');
+
+  constructor(private store: Store<{ counter: { counter: number } }>) {
+    this.count$ = store.select(state => state.counter.counter);
+  }
+
+  ngOnDestroy() {
+    if (this.counterServiceSub) {
+      this.counterServiceSub.unsubscribe();
+    }
   }
 }
--- src/app/store/reducers/counter.reducer.ts
+++ src/app/store/reducers/counter.reducer.ts
@@ -2,12 +2,19 @@ import { createReducer, on } from '@ngrx/store';
 // import { Action } from '@ngrx/store';
 // import { CounterActions, INCREMENT, IncrementAction } from '../actions/counter.actions';
 import { decrement, increment, reset } from '../actions/counter.actions';
-const initialState = 0;
+const initialState = { counter: 0 };
+
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state, action) => state + action.value),
-  on(decrement, (state, action) => state - action.value),
-  on(reset, (state) => (state = initialState))
+  on(increment, (state, action) => ({
+    ...state,
+    counter: state.counter + action.value
+  })),
+  on(decrement, (state, action) => ({
+    ...state,
+    counter: state.counter - action.value
+  })),
+  on(reset, (state) => Object.assign({}, state, { counter: initialState.counter }))
 );
 
 // I am using alternative way to to create reducer and handle increment action
