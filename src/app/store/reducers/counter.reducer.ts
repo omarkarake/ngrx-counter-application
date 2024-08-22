65b0cd5324f806b6e8b1c90364407b620a810c67
omar karake <omar.karake@amalitechtraining.org>
2024-08-22 16:43:35 +0200

    reverting codes back to create actions and create reducers

--- src/app/store/reducers/counter.reducer.ts
+++ src/app/store/reducers/counter.reducer.ts
@@ -1,16 +1,17 @@
-import { Action, createReducer, on } from '@ngrx/store';
-import { CounterActions, INCREMENT, IncrementAction } from '../actions/counter.actions';
-// import { increment } from '../actions/counter.actions';
+import { createReducer, on } from '@ngrx/store';
+// import { Action } from '@ngrx/store';
+// import { CounterActions, INCREMENT, IncrementAction } from '../actions/counter.actions';
+import { increment } from '../actions/counter.actions';
 const initialState = 0;
-// export const counterReducer = createReducer(
-//   initialState,
-//   on(increment, (state, action:CounterActions) => state + action.value)
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state, action) => state + action.value)
+);
 
 // I am using alternative way to to create reducer and handle increment action
-export function counterReducer(state = initialState, action: CounterActions | Action) {
-  if (action.type === INCREMENT) {
-    return state + (action as IncrementAction).value;
-  }
-  return state;
-}
+// export function counterReducer(state = initialState, action: CounterActions | Action) {
+//   if (action.type === INCREMENT) {
+//     return state + (action as IncrementAction).value;
+//   }
+//   return state;
+// }
