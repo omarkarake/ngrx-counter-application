1668475610452baa186e309cbade6a64ddeeb177
omar karake <omar.karake@amalitechtraining.org>
2024-08-22 15:40:55 +0200

    chore: using an alternative way to handle creation of reducer and handling action payload

--- src/app/store/reducers/counter.reducer.ts
+++ src/app/store/reducers/counter.reducer.ts
@@ -1,11 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
 import { increment } from '../actions/counter.actions';
 const initialState = 0;
-export const counterReducer = createReducer(
-  initialState,
-  on(increment, (state, action) => state + action.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (state, action) => state + action.value)
+// );
 
-// export function counterReducer(state = initialState){
-//     return state;
-// }
+// I am using alternative way to to create reducer and handle increment action
+export function counterReducer(state = initialState, action: any) {
+  if (action.type === '[Counter] Increment') {
+    return state + action.value;
+  }
+  return state;
+}
